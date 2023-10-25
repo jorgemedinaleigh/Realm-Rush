a5fc025ea896e54771f599a8d6d318f8b044de28
Jorge Medina <jorgemedinaleigh@protonmail.com>
2023-10-25 22:00:01 +0100

    UI addition
    -Added gold balance display to the UI
    -Added adequate font

--- Assets/Scripts/BankController.cs
+++ Assets/Scripts/BankController.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BankController : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI displayBalance;
     [SerializeField] int startingBalance = 150;
     int currentBalance;
 
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public int GetCurrentBalance()
@@ -21,16 +24,23 @@ public class BankController : MonoBehaviour
     public void Deposit(int amount)
     {
         currentBalance = currentBalance + Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance = currentBalance - Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalance <= 0)
         {
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
